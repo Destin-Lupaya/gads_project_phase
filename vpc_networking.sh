66902832933f2bd92f280aedc9bf2346abace398
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2022-11-30 20:07:56 +0200

    vpc test

--- vpc_networking.sh
+++ vpc_networking.sh
@@ -85,3 +85,50 @@ Convert the network to a custom mode network
     "projects/qwiklabs-gcp-01-ebb257e17d6d/regions/europe-central2/subnetworks/mynetwork"
   ]
 }
+
+
+#Create the managementnet network
+gcloud compute networks create managementnet --project=qwiklabs-gcp-01-ebb257e17d6d --subnet-mode=custom --mtu=1460 --bgp-routing-mode=regional
+
+gcloud compute networks subnets create managementsubnet-us --project=qwiklabs-gcp-01-ebb257e17d6d --range=10.240.0.0/20 --stack-type=IPV4_ONLY --network=managementnet --region=us-east4
+
+#Create the privatenet network Create the privatenet network using the gcloud command line. In the Cloud Console, click Activate Cloud Shell
+gcloud compute networks create privatenet --subnet-mode=custom
+
+gcloud compute networks subnets create privatesubnet-us --network=privatenet --region=us-east4 --range=172.16.0.0/24
+
+gcloud compute networks subnets create privatesubnet-eu --network=privatenet --region=europe-west1 --range=172.20.0.0/20
+
+gcloud compute networks list
+
+gcloud compute networks subnets list --sort-by=NETWORK
+
+#Create the firewall rules for managementnet Create firewall rules to allow SSH, ICMP, and RDP ingress traffic to VM instances on the managementnet network.
+
+gcloud compute --project=qwiklabs-gcp-01-ebb257e17d6d firewall-rules create managementnet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=managementnet --action=ALLOW --rules=tcp:22,tcp:3389,icmp --source-ranges=0.0.0.0/0
+
+#Create the firewall rules for privatenet Create the firewall rules for privatenet network using the gcloud command line.
+
+gcloud compute firewall-rules create privatenet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=privatenet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
+
+gcloud compute firewall-rules list --sort-by=NETWORK
+
+#Create the managementnet-us-vm instance
+gcloud compute instances create managementnet-us-vm --project=qwiklabs-gcp-01-ebb257e17d6d --zone=us-east4-b --machine-type=e2-micro --network-interface=network-tier=PREMIUM,subnet=managementsubnet-us --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=1028655937043-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=managementnet-us-vm,image=projects/debian-cloud/global/images/debian-11-bullseye-v20221102,mode=rw,size=10,type=projects/qwiklabs-gcp-01-ebb257e17d6d/zones/us-east4-b/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
+
+#Create the privatenet-us-vm instance Create the privatenet-us-vm instance using the gcloud command line. Return to Cloud Shell. If necessary, click Activate Cloud Shell
+
+gcloud compute instances create privatenet-us-vm --zone=us-east4-b --machine-type=e2-micro --subnet=privatesubnet-us --image-family=debian-11 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=privatenet-us-vm
+
+gcloud compute instances list --sort-by=ZONE
+
+#Task 4. Explore the connectivity across networks
+
+ping -c 3 34.79.19.164
+ping -c 3 10.132.0.2
+
+ping -c 3 10.132.0.2
+
+ping -c 3 10.132.0.2
+
+ping -c 3 10.132.0.2
