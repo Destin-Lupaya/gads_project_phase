05d877080fae83cd6e6de6af2baf0ab0eddcb8b6
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2022-12-02 17:45:59 +0200

    Cloud IAM

--- Lab Cloud IAM.sh
+++ Lab Cloud IAM.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 #rachelkabuyre@gmail.com
+
+gsutil ls gs://[YOUR_BUCKET_NAME]
+
+#Use the Service Account User
+gcloud compute instances list
+
+gsutil cp gs://[YOUR_BUCKET_NAME]/sample.txt 
+
+mv sample.txt sample2.txt
+
+gsutil cp sample2.txt gs://[YOUR_BUCKET_NAME]
+
+gsutil cp sample2.txt gs://[YOUR_BUCKET_NAME]
