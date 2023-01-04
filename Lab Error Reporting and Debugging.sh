b7ddfec11c2c60e1f40db44f8145a2c345ad9abc
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-01-04 15:57:52 +0200

    create

--- Lab Error Reporting and Debugging.sh
+++ Lab Error Reporting and Debugging.sh
@@ -10,3 +10,27 @@
 mkdir appengine-hello
 cd appengine-hello
 gsutil cp gs://cloud-training/archinfra/gae-hello/* .
+
+# To run the application using the local development server in Cloud Shell, run the following command:
+
+dev_appserver.py $(pwd)
+
+# In Cloud Shell, click Web Preview > Preview on port 8080 to view the application. You may have to collapse the Navigation menu pane to access the Web Preview icon.
+# Note: A new browser window opens to the localhost and displays the message Hello, World!
+# In Cloud Shell, press Ctrl+C to exit the development server.
+
+# Deploy the application to App Engine
+# To deploy the application to App Engine, run the following command:
+
+gcloud app deploy app.yaml
+
+# If prompted for a region, enter the number corresponding to a region.
+
+# When prompted, type Y to continue.
+
+# When the process is done, verify that the application is working by running the following command:
+
+gcloud app browse
+
+# Note: If Cloud Shell does not detect your browser, click the link in the Cloud Shell output to view your app. You might have to refresh the page for the application to load.
+# If needed, press Ctrl+C to exit the development mode.
