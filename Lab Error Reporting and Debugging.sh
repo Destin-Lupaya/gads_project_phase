f9abbce2de8f493cdd95ccd764a157fdf81efb30
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-01-04 15:55:35 +0200

    Task 1. Create an application

--- Lab Error Reporting and Debugging.sh
+++ Lab Error Reporting and Debugging.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 #rachelkabuyre@gmail.com
+# Task 1. Create an application
+# Get and test the application
+# In the Cloud Console, launch Cloud Shell by clicking Activate Cloud Shell ( Activate Cloud Shell icon). If prompted, click Continue.
+
+# To create a local folder and get the App Engine Hello world application, run the following commands:
+
+mkdir appengine-hello
+cd appengine-hello
+gsutil cp gs://cloud-training/archinfra/gae-hello/* .
