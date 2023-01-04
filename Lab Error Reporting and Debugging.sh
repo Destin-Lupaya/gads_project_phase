8becaf77ae8ed335262792edd3e3a8526a6b19cd
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-01-04 16:06:03 +0200

    Introduce an error to break the application

--- Lab Error Reporting and Debugging.sh
+++ Lab Error Reporting and Debugging.sh
@@ -34,3 +34,34 @@ gcloud app browse
 
 # Note: If Cloud Shell does not detect your browser, click the link in the Cloud Shell output to view your app. You might have to refresh the page for the application to load.
 # If needed, press Ctrl+C to exit the development mode.
+
+# Introduce an error to break the application
+# To examine the main.py file, run the following command:
+
+cat main.py
+
+# Note: Notice that the application imports webapp2.
+
+# You will break the configuration by replacing the import library with one that doesn't exist.
+# To use the sed stream editor to change the import library to the nonexistent webapp22, run the following command:
+
+sed -i -e 's/webapp2/webapp22/' main.py
+
+# To verify the change you made in the main.py file, run the following command:
+
+cat main.py
+
+# Note: Notice that the application now tries to import webapp22.
+# Re-deploy the application to App Engine
+# To re-deploy the application to App Engine, run the following command:
+
+gcloud app deploy app.yaml --quiet
+
+# Note: The --quiet flag disables all interactive prompts when running gcloud commands. If input is required, defaults will be used. In this case, it avoids the need for you to type Y when prompted to continue the deployment.
+# When the process is done, verify that the application is broken by running the following command:
+
+gcloud app browse
+
+# Note: If Cloud Shell does not detect your browser, click the link in the Cloud Shell output to view your app.
+# If needed, press Ctrl+C to exit development mode.
+# Leave Cloud Shell open.
