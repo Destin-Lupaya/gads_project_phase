29770a10c66e425d07a565e163521444f2087065
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-01-04 14:59:58 +0200

    Task 4. Analyze a large billing dataset with SQL.

--- Lab Examining Billing data with BigQuery.sh
+++ Lab Examining Billing data with BigQuery.sh
@@ -91,5 +91,163 @@
 
 SELECT * FROM `imported_billing_data.sampleinfotable`
 WHERE Cost > 0
-Copied!
-Click Run.
+
+# Click Run.
+
+# Task 4. Analyze a large billing dataset with SQL
+# In the next activity, you use BigQuery to analyze a sample dataset with 22,537 lines of billing data.
+
+# Note: The cloud-training-prod-bucket.arch_infra.billing_data dataset used in this task is shared with the public. For more information on public datasets and how to share datasets with the public, refer to the BigQuery public datasets Guide.
+# For New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  resource_type,
+  start_time,
+  end_time,
+  cost,
+  project_id,
+  project_name,
+  project_labels_key,
+  currency,
+  currency_conversion_rate,
+  usage_amount,
+  usage_unit
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+
+# Click Run. Verify that the resulting table has 22,537 lines of billing data.
+
+# To find the latest 100 records where there were charges (cost > 0), for New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  resource_type,
+  start_time,
+  end_time,
+  cost,
+  project_id,
+  project_name,
+  project_labels_key,
+  currency,
+  currency_conversion_rate,
+  usage_amount,
+  usage_unit
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+WHERE
+  Cost > 0
+ORDER BY end_time DESC
+LIMIT
+  100
+
+# Click Run.
+
+# To find all charges that were more than 3 dollars, for Compose New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  resource_type,
+  start_time,
+  end_time,
+  cost,
+  project_id,
+  project_name,
+  project_labels_key,
+  currency,
+  currency_conversion_rate,
+  usage_amount,
+  usage_unit
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+WHERE
+  cost > 3
+
+# Click Run.
+
+# To find the product with the most records in the billing data, for New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  COUNT(*) AS billing_records
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+GROUP BY
+  product
+ORDER BY billing_records DESC
+
+# Click Run.
+
+# To find the most frequently used product costing more than 1 dollar, for New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  COUNT(*) AS billing_records
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+WHERE
+  cost > 1
+GROUP BY
+  product
+ORDER BY
+  billing_records DESC
+
+# Click Run.
+
+# Which product had the most billing records of over $1
+
+# Compute Engine has 17 charges costing more than 1 dollar.
+
+# Kubernetes Engine has 7 charges costing more than 1 dollar.
+
+# Cloud SQL has 15 charges costing more than 1 dollar.
+
+# To find the most commonly charged unit of measure, for Compose New Query, paste the following in Query Editor:
+
+SELECT
+  usage_unit,
+  COUNT(*) AS billing_records
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+WHERE cost > 0
+GROUP BY
+  usage_unit
+ORDER BY
+  billing_records DESC
+
+# Click Run.
+
+# What was the most commonly charged unit of measure?
+
+# Requests were the most commonly charged unit of measure with 6,539 requests.
+
+# Requests were the most commonly charged unit of measure with 504 requests.
+
+# Byte-seconds were the most commonly charged unit of measure with 2,937 requests.
+
+# To find the product with the highest aggregate cost, for New Query, paste the following in Query Editor:
+
+SELECT
+  product,
+  ROUND(SUM(cost),2) AS total_cost
+FROM
+  `cloud-training-prod-bucket.arch_infra.billing_data`
+GROUP BY
+  product
+ORDER BY
+  total_cost DESC
+
+# Click Run.
+
+# Which product has the highest total cost?
+
+# Compute Engine has an aggregate cost of $112.02.
+
+# Cloud SQL has an aggregate cost of $47.37
+
+# BigQuery has an aggregate cost of $114.02
+
+# Task 5. Review
+# In this lab, you imported billing data into BigQuery that had been generated as a CSV file. You ran a simple query on the file. Then you accessed a shared dataset containing more than 22,000 records of billing information. You ran a variety of queries on that data to explore how you can use BigQuery to ask and answer questions by running queries.
+
+# End your lab
