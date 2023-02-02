7c0365bc907fa836717cd55447358a83f68d41ee
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-02-02 16:33:51 +0200

    Run and deploy a container

--- Google Cloud Fundamentals Getting Started with GKE.sh
+++ Google Cloud Fundamentals Getting Started with GKE.sh
@@ -42,22 +42,22 @@
 
 # For convenience, place the zone that Qwiklabs assigned to you into an environment variable called MY_ZONE. At the Cloud Shell prompt, type this partial command:
 
-# export MY_ZONE=
-# Copied!
+export MY_ZONE=
+
 # followed by the zone that Qwiklabs assigned to you. Your complete command will look similar to this:
 
-# export MY_ZONE=us-central1-a
-# Copied!
+export MY_ZONE=us-central1-a
+
 # Start a Kubernetes cluster managed by Kubernetes Engine. Name the cluster webfrontend and configure it to run 2 nodes:
 
-# gcloud container clusters create webfrontend --zone $MY_ZONE --num-nodes 2
-# Copied!
+gcloud container clusters create webfrontend --zone $MY_ZONE --num-nodes 2
+
 # It takes several minutes to create a cluster as Kubernetes Engine provisions virtual machines for you.
 
 # After the cluster is created, check your installed version of Kubernetes using the kubectl version command:
 
-# kubectl version
-# Copied!
+kubectl version
+
 # The gcloud container clusters create command automatically authenticated kubectl for you.
 
 # View your running nodes in the GCP Console. On the Navigation menu (Navigation menu icon), click Compute Engine > VM Instances.
@@ -70,25 +70,25 @@
 # Task 4. Run and deploy a container
 # From your Cloud Shell prompt, launch a single instance of the nginx container. (Nginx is a popular web server.)
 
-# kubectl create deploy nginx --image=nginx:1.17.10
-# Copied!
+kubectl create deploy nginx --image=nginx:1.17.10
+
 # In Kubernetes, all containers run in pods. This use of the kubectl create command caused Kubernetes to create a deployment consisting of a single pod containing the nginx container. A Kubernetes deployment keeps a given number of pods up and running even in the event of failures among the nodes on which they run. In this command, you launched the default number of pods, which is 1.
 
 # Note: If you see any deprecation warning about future versions, you can simply ignore it for now and can proceed further.
 # View the pod running the nginx container:
 
-# kubectl get pods
-# Copied!
+kubectl get pods
+
 # Expose the nginx container to the Internet:
 
-# kubectl expose deployment nginx --port 80 --type LoadBalancer
-# Copied!
+kubectl expose deployment nginx --port 80 --type LoadBalancer
+
 # Kubernetes created a service and an external load balancer with a public IP address attached to it. The IP address remains the same for the life of the service. Any network traffic to that public IP address is routed to pods behind the service: in this case, the nginx pod.
 
 # View the new service:
 
-# kubectl get services
-# Copied!
+kubectl get services
+
 # You can use the displayed external IP address to test and contact the nginx container remotely.
 
 # It may take a few seconds before the External-IP field is populated for your service. This is normal. Just re-run the kubectl get services command every few seconds until the field is populated.
@@ -97,18 +97,18 @@
 
 # Scale up the number of pods running on your service:
 
-# kubectl scale deployment nginx --replicas 3
-# Copied!
+kubectl scale deployment nginx --replicas 3
+
 # Scaling up a deployment is useful when you want to increase available resources for an application that is becoming more popular.
 
 # Confirm that Kubernetes has updated the number of pods:
 
-# kubectl get pods
-# Copied!
+kubectl get pods
+
 # Confirm that your external IP address has not changed:
 
-# kubectl get services
-# Copied!
+kubectl get services
+
 # Return to the web browser tab in which you viewed your cluster's external IP address. Refresh the page to confirm that the nginx web server is still responding.
 
 # Click Check my progress to verify the objective.
